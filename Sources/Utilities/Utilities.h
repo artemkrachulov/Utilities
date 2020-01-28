e24c75ed03debdcaceaa536a443af9b6186b6699
Artem Krachulov <artem.krachulov@gmail.com>
2020-01-28 12:21:13 +0200

    Initial commit

---
+++ Sources/Utilities/Utilities.h
@@ -0,0 +1,19 @@
+//
+//  Utilities.h
+//  Utilities
+//
+//  Created by Artem on 24.12.2019.
+//  Copyright © 2019 Artem Krachulov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Utilities.
+FOUNDATION_EXPORT double UtilitiesVersionNumber;
+
+//! Project version string for Utilities.
+FOUNDATION_EXPORT const unsigned char UtilitiesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Utilities/PublicHeader.h>
+
+
